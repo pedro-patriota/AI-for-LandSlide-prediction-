8c54ca1445074c9c697fb5139dcbefee458a7db7
Pedro <93168925+pedro-patriota@users.noreply.github.com>
2024-11-11 16:59:24 -0300

    refactored method from merge tables

--- processing/merge/merge_tables.py
+++ processing/merge/merge_tables.py
@@ -8,6 +8,7 @@ from base.pandas_constants import (
     ValuesConstants,
 )
 
+DELIMITER_SEMICOLON = ';'
 
 class MergeTables:
     """
@@ -15,34 +16,30 @@ class MergeTables:
     """
 
     @staticmethod
-    def filter_confirmed_occurrences(df_calls_and_types: DataFrame) -> DataFrame:
-        df_calls_and_types = df_calls_and_types[
-            df_calls_and_types[DataFrameConstants.SOLICITACAO_DESCRICAO] != ValuesConstants.TESTES
-            ]
-
-        is_confirmed = []
-        for process in df_calls_and_types[DataFrameConstants.PROCESSO_OCORRENCIA]:
-            if process == ValuesConstants.DESLIZAMENTOS_DE_BARREIRAS:
-                is_confirmed.append(ProcessingConstants.CONFIRMED)
-            elif process == ValuesConstants.NAO_HA_OCORRENCIAS:
-                is_confirmed.append(ProcessingConstants.MAYBE_CONFIRMED)
-            else:
-                is_confirmed.append(ProcessingConstants.NOT_CONFIRMED)
-
-        df_calls_and_types[DataFrameConstants.IS_CONFIRMED] = is_confirmed
-
-        return df_calls_and_types.sort_values(DataFrameConstants.IS_CONFIRMED, ascending=False)
+    def mark_confirmation_status(process: str) -> int:
+        if process == ValuesConstants.DESLIZAMENTOS_DE_BARREIRAS:
+            return ProcessingConstants.CONFIRMED
+        elif process == ValuesConstants.NAO_HA_OCORRENCIAS:
+            return ProcessingConstants.MAYBE_CONFIRMED
+        else:
+            return ProcessingConstants.NOT_CONFIRMED
 
     @staticmethod
-    def merge_tables(df_occurrences: DataFrame, df_types: DataFrame):
-        df_calls_and_types = merge(df_occurrences, df_types)
-        df_calls_and_types_after_filters = MergeTables.filter_confirmed_occurrences(df_calls_and_types)
+    def filter_confirmed_occurrences(df: DataFrame) -> DataFrame:
+        df = df[df[DataFrameConstants.SOLICITACAO_DESCRICAO] != ValuesConstants.TESTES]
+        df[DataFrameConstants.IS_CONFIRMED] = df[DataFrameConstants.PROCESSO_OCORRENCIA].apply(
+            MergeTables.mark_confirmation_status
+        )
+        return df.sort_values(DataFrameConstants.IS_CONFIRMED, ascending=False)
 
-        df_calls_and_types_after_filters.to_csv(PathConstants.LOCATION_MERGED, index=False, header=True)
+    @staticmethod
+    def merge_tables(df_occurrences: DataFrame, df_types: DataFrame) -> None:
+        df_merged = merge(df_occurrences, df_types)
+        df_filtered = MergeTables.filter_confirmed_occurrences(df_merged)
+        df_filtered.to_csv(PathConstants.LOCATION_MERGED, index=False, header=True)
 
 
 if __name__ == '__main__':
     df_occurrences = read_csv(FilesConstants.OCCURRENCES)
-    df_types = read_csv(FilesConstants.OCCURRENCES_TYPES, delimiter=';')
-
+    df_types = read_csv(FilesConstants.OCCURRENCES_TYPES, delimiter=DELIMITER_SEMICOLON)
     MergeTables.merge_tables(df_occurrences, df_types)
